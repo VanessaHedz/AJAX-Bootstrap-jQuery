69c823a17604d85a8a2c24652d778b56b91562bb
Vanessa Mariana Hernández Chagoyán <vanessa.hernandez@mineria.unam.mx>
2024-08-27 18:53:49 -0600

    El botón pregunta si se elimina o no un elemento

--- CRUDS/jQuery.js
+++ CRUDS/jQuery.js
@@ -1,3 +1,5 @@
+const { timers } = require("jquery");
+
 $(document).ready(function(){
 });
 //Crear una nueva película
@@ -49,19 +51,34 @@ $("#btnSend").click(function () {
 });
 
 function deletePeliculas(id) {
-  $.ajax({
-    type: "post",
-    url: "delete.php",
-    dataType: "json",
-    data: {
-      id: id,
-    },
-    success: function (data) {
-      alert(data.id);
-      window.location.reload();
-    },
-    error: function () {
-      alert("error");
-    },
-  });
+
+    Swal.fire({
+        title: "¿Seguro que quieres eliminar la película?",
+        showDenyButton: true,
+        confirmButtonText: "Eliminar",
+        denyButtonText: 'No eliminar'
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          $.ajax({
+            type: "post",
+            url: "delete.php",
+            dataType: "json",
+            data: {
+              id: id,
+            },
+            success: function (data) {
+                Swal.fire("El elemento se eliminó correctamente", "", "success");
+                timer:1000,
+                window.location.reload();
+            },
+            error: function () {
+              alert("error");
+            },
+          });
+
+        } else if (result.isDenied) {
+          Swal.fire("El elemento no se eliminó", "", "info");
+        }
+      });
 }
