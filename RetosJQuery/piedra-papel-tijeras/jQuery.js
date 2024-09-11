715d25559fe60f903d2013dfedbb3b7c4f135e4e
Vanessa Mariana Hernández Chagoyán <vanessa.hernandez@mineria.unam.mx>
2024-09-11 12:10:44 -0600

    El mismo juego pero un poco más sofisticado

--- RetosJQuery/piedra-papel-tijeras/jQuery.js
+++ RetosJQuery/piedra-papel-tijeras/jQuery.js
@@ -1,63 +1,75 @@
 let jugador;
+let flgJugador = false;
+$("#btnOk").hide();
 
 $("#btnPiedra").click(function () {
   jugador = 1;
-  alert("Has seleccionado Piedra");
+  $("#divMensaje").append("<h1>Has seleccionado Piedra</h1>");
+  flgJugador = true;
 });
 
 $("#btnPapel").click(function () {
   jugador = 2;
-  alert("Has seleccionado Papel");
+  $("#divMensaje").append("<h1>Has seleccionado Papel</h1>");
+  flgJugador = true;
 });
 
 $("#btnTijeras").click(function () {
   jugador = 3;
-  alert("Has seleccionado Tijeras");
+  $("#divMensaje").append("<h1>Has seleccionado Tijeras</h1>");
+  flgJugador = true;
 });
 
 $("#btnJugar").click(function () {
-  let opciones = ["Piedra", "Papel", "Tijera"];
-  let rndm = Math.floor(Math.random() * 3 + 1); //Función para obtener un número aleatorio entre 1 - 3
-  /*  1: piedra
-      2: papel
-      3: tijeras
-    */
-  let msj = "";
-
-  switch (rndm) {
-    case 1:
-      msj = "Piedra";
-      break;
-    case 2:
-      msj = "Papel";
-      break;
-    case 3:
-      msj = "Tijeras";
-      break;
-  }
-
-  msj = "Yo escogí " + msj + ".";
-
-  if (jugador == rndm) {
-    msj = msj + " ¡Es un empate!";
-  } else if (jugador == 1) {
-    if (rndm == 2) {
-      msj = msj + " ¡Yo gané!";
-    } else {
-      msj = msj + " ¡Ganaste!";
-    }
-  } else if (jugador == 2) {
-    if (rndm == 1) {
-      msj = msj + " ¡Ganaste!";
-    } else {
-      msj = msj + " ¡Yo gané!";
-    }
+  if (!flgJugador) {
+    alert("Selecciona una opción para jugar");
   } else {
-    if (rndm == 1) {
-      msj = msj + " ¡Yo gané!";
+    $("#btnOk").show();
+    let opciones = ["Piedra", "Papel", "Tijeras"];
+    let rndm = Math.floor(Math.random() * 3 + 1); //Función para obtener un número aleatorio entre 1 - 3
+    /*1: piedra
+      2: papel
+      3: tijeras*/
+    let msj = opciones[rndm - 1];
+
+    msj = "Yo escogí " + msj + ".";
+
+    if (jugador == rndm) {
+      msj = msj + " ¡Es un empate!";
+    } else if (jugador == 1) {
+      if (rndm == 2) {
+        msj = msj + " ¡Yo gané!";
+      } else {
+        msj = msj + " ¡Ganaste!";
+      }
+    } else if (jugador == 2) {
+      if (rndm == 1) {
+        msj = msj + " ¡Ganaste!";
+      } else {
+        msj = msj + " ¡Yo gané!";
+      }
     } else {
-      msj = msj + " ¡Ganaste!";
+      if (rndm == 1) {
+        msj = msj + " ¡Yo gané!";
+      } else {
+        msj = msj + " ¡Ganaste!";
+      }
     }
+    // alert(msj);
+    $("#divMensaje").append("<h1>" + msj + "</h1>");
+
+    $("#btnOk").click(function () {
+      $("#btnJugar").show();
+      $("#btnOk").hide();
+      $("#divMensaje").empty();
+      flgJugador = false;
+    });
+
+    setTimeout(function () {
+      $("#btnJugar").show();
+      $("#btnOk").hide();
+      $("#divMensaje").empty();
+      flgJugador = false;
+    }, 5000);
   }
-  alert(msj);
 });
