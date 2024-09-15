306210869f869755fb9dfbff4eea6136eb48abd0
Vanessa Hernández <vanessahedzch@gmail.com>
2024-09-15 11:19:39 -0600

    Ya se ve quién fue el ganador. Se agregó un marcador.

--- RetosJQuery/emulador-bingo/jQuery.js
+++ RetosJQuery/emulador-bingo/jQuery.js
@@ -1,10 +1,16 @@
 let temp; //Ficha temporal aleatoria que se saca al tocar el botón
 let fichas = []; //Lista de fichas que ya se sacaron
 let cnt = 0; //Contador para poner diferentes renglones en las Fichas Pasadas
-let tempPlayer = 0, tempCPU = 0; 
-let Player = [], CPU=[];
+let tempPlayer = 0, tempCPU = 0;
+let Player = [], CPU = [];
+let arrayWin = ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*"];
+let MarcadorJugador = 0, MarcadorCPU =0;
 
 $("#document").ready(function () {
+
+  $("#btnAgain").hide();
+  $("#btnFichas").show();
+
   //Llenado de los tableros
   for (let n = 0; n < 15; n++) { //Celdas
     let flgPlayer = true, flgCPU = true;
@@ -29,8 +35,8 @@ $("#document").ready(function () {
       $("#tbJugador").append("<tr></tr>");
       $("#tbCPU").append("<tr></tr>");
     }
-    $("#tbJugador").append("<td id='cPlayer"+n+"' class='text-center'>" + Player[n] + "</td>");
-    $("#tbCPU").append("<td id='cCPU"+n+"' class='text-center'>" + CPU[n] + "</td>");
+    $("#tbJugador").append("<td id='cPlayer" + n + "' class='text-center'>" + Player[n] + "</td>");
+    $("#tbCPU").append("<td id='cCPU" + n + "' class='text-center'>" + CPU[n] + "</td>");
   }
 })
 
@@ -56,7 +62,7 @@ $("#btnFichas").click(function () {
       title: "La ficha que sacaste es:",
       text: temp,
       showConfirmButton: false,
-      timer: 2000,
+      timer: 500,
     });
 
     //Cada 10 fichas, cambiar de renglón
@@ -68,13 +74,39 @@ $("#btnFichas").click(function () {
   }
 
   // Ver si la ficha estuvo en el tablero del jugador o del CPU
-  if(Player.includes(temp)){
-    let num=$.inArray(temp,Player);
-    $("#cPlayer"+num).css("background-color","red");
+  if (Player.includes(temp)) {
+    let num = $.inArray(temp, Player);
+    $("#cPlayer" + num).css("background-color", "red");
+    $("#cPlayer" + num).css("border", "2px solid red");
+    Player[num] = "*";
+    console.log(Player);
   }
-  if(CPU.includes(temp)){
-    let num=$.inArray(temp,CPU);
-    $("#cCPU"+num).css("background-color","red");
+  if (CPU.includes(temp)) {
+    let num = $.inArray(temp, CPU);
+    $("#cCPU" + num).css("background-color", "red");
+    $("#cCPU" + num).css("border", "2px solid red");
+    CPU[num] = "*";
+    console.log(CPU);
   }
+
+  if (Player[0] == "*" && Player[1] == "*" && Player[2] == "*" && Player[3] == "*" && Player[4] == "*" ||
+    Player[5] == "*" && Player[6] == "*" && Player[7] == "*" && Player[8] == "*" && Player[9] == "*" ||
+    Player[10] == "*" && Player[11] == "*" && Player[12] == "*" && Player[13] == "*" && Player[14] == "*") {
+    alert("El jugador ganó");
+    MarcadorJugador++;
+    $("#btnFichas").hide();
+    $("#btnAgain").show();
+  }
+  else if (CPU[0] == "*" && CPU[1] == "*" && CPU[2] == "*" && CPU[3] == "*" && CPU[4] == "*" ||
+    CPU[5] == "*" && CPU[6] == "*" && CPU[7] == "*" && CPU[8] == "*" && CPU[9] == "*" ||
+    CPU[10] == "*" && CPU[11] == "*" && CPU[12] == "*" && CPU[13] == "*" && CPU[14] == "*") {
+    alert("El CPU ganó");
+    MarcadorCPU++;
+    $("#btnFichas").hide();
+    $("#btnAgain").show();
+  }
+
+  $("#h3MarcadorJugador").text(MarcadorJugador);
+  $("#h3MarcadorCPU").text(MarcadorCPU);
 
 });
