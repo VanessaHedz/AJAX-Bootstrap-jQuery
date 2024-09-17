fbc95d77f8de483a00271a8a37c71e9b00fd2fbd
vanessa.hernandez <vanessa.hernandez@mineria.unam.mx>
2024-09-17 13:53:09 -0600

    El juego ya funciona correctamente

--- RetosJQuery/emulador-bingo/jQuery.js
+++ RetosJQuery/emulador-bingo/jQuery.js
@@ -1,44 +1,14 @@
 let temp; //Ficha temporal aleatoria que se saca al tocar el botón
 let fichas = []; //Lista de fichas que ya se sacaron
 let cnt = 0; //Contador para poner diferentes renglones en las Fichas Pasadas
-let tempPlayer = 0, tempCPU = 0;
 let Player = [], CPU = [];
-let arrayWin = ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*"];
-let MarcadorJugador = 0, MarcadorCPU =0;
+let tempPlayer = 0, tempCPU = 0;
+
+let MarcadorJugador = 0, MarcadorCPU = 0;
 
 $("#document").ready(function () {
-
-  $("#btnAgain").hide();
-  $("#btnFichas").show();
-
-  //Llenado de los tableros
-  for (let n = 0; n < 15; n++) { //Celdas
-    let flgPlayer = true, flgCPU = true;
-
-    // ---------------------- CREACIÓN DE LA LISTA DE FICHAS EN EL TABLERO
-    while (flgPlayer) {
-      tempPlayer = Math.floor(Math.random() * 90 + 1);
-      flgPlayer = Player.includes(tempPlayer);
-    }
-    while (flgCPU) {
-      tempCPU = Math.floor(Math.random() * 90 + 1);
-      flgCPU = CPU.includes(tempCPU);
-    }
-
-    Player.push(tempPlayer);
-    CPU.push(tempCPU);
-    // ----------------------
-
-    tempPlayer = Math.floor(Math.random() * 90 + 1);
-    tempCPU = Math.floor(Math.random() * 90 + 1);
-    if (n % 5 == 0) {
-      $("#tbJugador").append("<tr></tr>");
-      $("#tbCPU").append("<tr></tr>");
-    }
-    $("#tbJugador").append("<td id='cPlayer" + n + "' class='text-center'>" + Player[n] + "</td>");
-    $("#tbCPU").append("<td id='cCPU" + n + "' class='text-center'>" + CPU[n] + "</td>");
-  }
-})
+  Juego();
+});
 
 $("#btnFichas").click(function () {
   let flgRepetido = true;
@@ -67,10 +37,12 @@ $("#btnFichas").click(function () {
 
     //Cada 10 fichas, cambiar de renglón
     //Insertar las fichas en el tablero
-    if ((fichas.length) % 10 == 1) {
+    if (fichas.length % 10 == 1) {
       $("#tbFichas").append("<tr></tr>");
     }
-    $("#tbFichas").append("<td class='text-center td--fichas'>" + temp + "</td>");
+    $("#tbFichas").append(
+      "<td class='text-center td--fichas'>" + temp + "</td>"
+    );
   }
 
   // Ver si la ficha estuvo en el tablero del jugador o del CPU
@@ -89,18 +61,35 @@ $("#btnFichas").click(function () {
     console.log(CPU);
   }
 
-  if (Player[0] == "*" && Player[1] == "*" && Player[2] == "*" && Player[3] == "*" && Player[4] == "*" ||
-    Player[5] == "*" && Player[6] == "*" && Player[7] == "*" && Player[8] == "*" && Player[9] == "*" ||
-    Player[10] == "*" && Player[11] == "*" && Player[12] == "*" && Player[13] == "*" && Player[14] == "*") {
-    alert("El jugador ganó");
+  if (
+    (Player[0] == "*" && Player[1] == "*" && Player[2] == "*" && Player[3] == "*" && Player[4] == "*") ||
+    (Player[5] == "*" && Player[6] == "*" && Player[7] == "*" && Player[8] == "*" && Player[9] == "*") ||
+    (Player[10] == "*" && Player[11] == "*" && Player[12] == "*" && Player[13] == "*" && Player[14] == "*")
+  ) {
+
+    Swal.fire({
+      title: "¡El jugador ganó!",
+      showConfirmButton: false,
+      timer: 1000,
+    })
+    
+    confetti();
+    
     MarcadorJugador++;
     $("#btnFichas").hide();
     $("#btnAgain").show();
-  }
-  else if (CPU[0] == "*" && CPU[1] == "*" && CPU[2] == "*" && CPU[3] == "*" && CPU[4] == "*" ||
-    CPU[5] == "*" && CPU[6] == "*" && CPU[7] == "*" && CPU[8] == "*" && CPU[9] == "*" ||
-    CPU[10] == "*" && CPU[11] == "*" && CPU[12] == "*" && CPU[13] == "*" && CPU[14] == "*") {
-    alert("El CPU ganó");
+  } else if (
+    (CPU[0] == "*" && CPU[1] == "*" && CPU[2] == "*" && CPU[3] == "*" && CPU[4] == "*") ||
+    (CPU[5] == "*" && CPU[6] == "*" && CPU[7] == "*" && CPU[8] == "*" && CPU[9] == "*") ||
+    (CPU[10] == "*" && CPU[11] == "*" && CPU[12] == "*" && CPU[13] == "*" && CPU[14] == "*")
+  ) {
+    
+    Swal.fire({
+      title: "¡El CPU ganó!",
+      showConfirmButton: false,
+      timer: 1000,
+    })
+    
     MarcadorCPU++;
     $("#btnFichas").hide();
     $("#btnAgain").show();
@@ -108,5 +97,59 @@ $("#btnFichas").click(function () {
 
   $("#h3MarcadorJugador").text(MarcadorJugador);
   $("#h3MarcadorCPU").text(MarcadorCPU);
-
 });
+
+$("#btnAgain").click(function(){
+  //Borra el tablero anterior
+  $("#tbJugador").find("tr").remove();
+  $("#tbCPU").find("tr").remove();
+  $("#tbJugador").find("td").remove();
+  $("#tbCPU").find("td").remove();
+
+  //Borra el tablero de las fichas pasadas
+  $("#tbFichas").find("tr").remove();
+  $("#tbFichas").find("td").remove();
+  fichas = [];
+
+  Juego();
+});
+
+// Funciones
+function Juego() {
+  Player = [];
+  CPU = [];
+
+  $("#btnAgain").hide();
+  $("#btnFichas").show();
+
+  for (let n = 0; n < 15; n++) {
+    //Celdas
+    let flgPlayer = true, flgCPU = true;
+
+    // CREACIÓN DE LA LISTA DE FICHAS EN EL TABLERO
+    while (flgPlayer) {
+      tempPlayer = Math.floor(Math.random() * 90 + 1);
+      flgPlayer = Player.includes(tempPlayer);
+    }
+    while (flgCPU) {
+      tempCPU = Math.floor(Math.random() * 90 + 1);
+      flgCPU = CPU.includes(tempCPU);
+    }
+
+    Player.push(tempPlayer);
+    CPU.push(tempCPU);
+    // ----------------------
+    tempPlayer = Math.floor(Math.random() * 90 + 1);
+    tempCPU = Math.floor(Math.random() * 90 + 1);
+    if (n % 5 == 0) {
+      $("#tbJugador").append("<tr></tr>");
+      $("#tbCPU").append("<tr></tr>");
+    }
+    $("#tbJugador").append(
+      "<td id='cPlayer" + n + "' class='text-center'>" + Player[n] + "</td>"
+    );
+    $("#tbCPU").append(
+      "<td id='cCPU" + n + "' class='text-center'>" + CPU[n] + "</td>"
+    );
+  }
+}
